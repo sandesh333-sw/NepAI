a6bf0d736a1a14d0a097902e2110101573a741bc
sandesh333-sw <sandeshpulami333@gmail.com>
2025-07-18 17:54:24 +0100

    Changes in Chat Feature

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,19 +1,35 @@
 import "./Chat.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { MyContext } from "./MyContext";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
 
-
 function Chat() {
-  const { newChat, prevChats } = useContext(MyContext);
+  const { newChat, prevChats, reply } = useContext(MyContext);
+  const [latestReply, setLatestReply] = useState(null);
+
+  useEffect(() => {
+    if (!prevChats?.length) return;
+
+    const content = reply.split(" ");
+
+    let idx = 0;
+    const interval = setInterval(() => {
+      setLatestReply(content.slice(0, idx + 1).join(""));
+
+      idx++;
+      if (idx >= content.length) clearInterval(interval);
+    }, 40);
+
+    return () => clearInterval(interval);
+  }, [prevChats, reply]);
 
   return (
     <>
       {newChat && <h1>Start a new chat!</h1>}
       <div className="chats">
-        {prevChats?.map((chat, idx) => (
+        {prevChats?.slice(0, -1).map((chat, idx) => (
           <div
             className={chat.role === "user" ? "userDiv" : "nepAIDiv"}
             key={idx}
@@ -21,10 +37,19 @@ function Chat() {
             {chat.role === "user" ? (
               <p className="userMessage">{chat.content}</p>
             ) : (
-              <ReactMarkdown rehypePlugins={rehypeHighlight}>{chat.content}</ReactMarkdown>
+              <ReactMarkdown rehypePlugins={rehypeHighlight}>
+                {chat.content}
+              </ReactMarkdown>
             )}
           </div>
         ))}
+        {prevChats.length > 0 && latestReply !== null && (
+          <div className="aiDiv" key={"typing"}>
+            <ReactMarkdown rehypePlugins={rehypeHighlight}>
+              {latestReply}
+            </ReactMarkdown>
+          </div>
+        )}
       </div>
     </>
   );
