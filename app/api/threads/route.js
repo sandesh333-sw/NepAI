2023c542f8d8b3618f5ce60db5363b2cd299cd04
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-02-03 16:00:27 +0000

    feat: create a get endpoint

---
+++ app/api/threads/route.js
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import dbConnect from "@/app/lib/dbConnect";
+import Thread from "@/app/models/Thread";
+
+export async function GET(){
+    try {
+        const { userId } = await auth();
+
+        if (!userId){
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        await dbConnect();
+
+        const threads = await Thread.find({ userId })
+          .sort({ updatedAt: -1})
+          .select('-messages')
+          .lean();
+
+          return NextResponse.json(threads);
+
+    } catch (error) {
+        console.error("Get threads error: ", error);
+        return NextResponse.json({ error: "Failed to fetch threads" }, { status: 500});
+    }
+}
