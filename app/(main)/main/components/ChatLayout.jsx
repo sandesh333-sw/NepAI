ca8457acf61cf8b1b68f98f29df5338abfdb3f72
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-02-04 15:18:08 +0000

    feat: restructuring chatsidebar and chatwindow

--- app/(main)/main/components/ChatLayout.jsx
+++ app/(main)/main/components/ChatLayout.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import ChatSidebar from './ChatSidebar'
+import ChatWindow from './ChatWindow'
 
 const ChatLayout = () => {
   return (
     <div>
       ChatLayout
+
+      <ChatSidebar />
+      <ChatWindow />
     </div>
   )
 }
