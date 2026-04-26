6b13775ad23d9ec60753e6b447dfd5692e8b1878
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-04-26 23:32:37 +0100

    feat: fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   output: "standalone",
   turbopack: {
     root: __dirname,
   },
-  /* config options here */
 };
 
 export default nextConfig;
